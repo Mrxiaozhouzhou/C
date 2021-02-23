a2e373590bc6cc1fc09b7a287f4d7cd0235c56c5
Mrxiaozhouzhou <63716129+Mrxiaozhouzhou@users.noreply.github.com>
2021-02-23 17:32:07 +0800

    Add files via upload

---
+++ day3.cpp
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+
+void printf(char n)
+{
+
+}
+
+int main()
+{
+	char arr[] = "baby";
+	printf(arr);
+}
+
+
+
+
+//void printf(int n)
+//{
+//	if (n>9)
+//	{
+//		printf(n/10);
+//	}
+//	printf("%d\n", n % 10);
+//}
+////printfµÝ¹é
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	printf( num);
+//}
+
+
+
+
+
+//int is_prime(int n)
+//{
+//	int a;
+//	int b;//Æ½·½¸ù
+//	b = (int)sqrt((double)n);
+//	for ( a = 2; a < n; a++)
+//	{
+//		if (n % a == 0)
+//			return 0;		
+//			
+//		
+//	}
+//	return 1;
+//}
+
+//int is_prime(int n)
+//{
+//	int a;
+//	for ( a = 2; a < n; a++)
+//	{
+//		if (n % a == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for ( i = 100; i < 200; i++)
+//	{
+//		if (is_prime(i) == 1)
+//			printf("%d\n", i);
+//	}
+//}
